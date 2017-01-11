220e33601dd4feda135818058da22a92bdc45f1f
unknown <crmadmin@W7E-PC-TRAIN.train.dom>
2017-01-11 15:59:23 +0200

    added FormModule to detail component

--- ng2app/src/app/detail/detail.component.ts
+++ ng2app/src/app/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+  myName: AbstractControl;
+
+  constructor(fb: FormBuilder) {
+
+    this.myForm = fb.group({
+      "myName": ['', Validators.required]
+    });
+
+    this.myName = this.myForm.controls["myName"];
+
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: any): void {
+    console.log("form", form);
   }
 
 }
